f186f7197486fc659ee4eff2fefd66b36d99fd82
David Davydov <kctrlv@gmail.com>
2017-01-24 15:23:43 -0700

    all finished

---
+++ 13_http_json_api_server.js
@@ -0,0 +1,66 @@
+var http = require('http')
+var url = require('url')
+
+var server = http.createServer(function (req, res) {
+  var requestUrl = url.parse(req.url, true)
+  var pathname = requestUrl.pathname
+  var iso = requestUrl.query['iso']
+  var date = new Date(iso)
+  var time = {
+    'hour': date.getHours(),
+    'minute': date.getMinutes(),
+    'second': date.getSeconds()
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+
+  if(pathname == '/api/parsetime') {
+    console.log(pathname)
+    res.end(JSON.stringify(time))
+  } else if (pathname == '/api/unixtime') {
+    console.log(pathname)
+    res.end(JSON.stringify({'unixtime': date.getTime()}))
+  }
+})
+
+server.listen(process.argv[2])
+
+
+
+// Official Solution:
+
+// var http = require('http')
+// var url = require('url')
+//
+// function parsetime (time) {
+//   return {
+//     hour: time.getHours(),
+//     minute: time.getMinutes(),
+//     second: time.getSeconds()
+//   }
+// }
+//
+// function unixtime (time) {
+//   return { unixtime: time.getTime() }
+// }
+//
+// var server = http.createServer(function (req, res) {
+//   var parsedUrl = url.parse(req.url, true)
+//   var time = new Date(parsedUrl.query.iso)
+//   var result
+//
+//   if (/^\/api\/parsetime/.test(req.url)) {
+//     result = parsetime(time)
+//   } else if (/^\/api\/unixtime/.test(req.url)) {
+//     result = unixtime(time)
+//   }
+//
+//   if (result) {
+//     res.writeHead(200, { 'Content-Type': 'application/json' })
+//     res.end(JSON.stringify(result))
+//   } else {
+//     res.writeHead(404)
+//     res.end()
+//   }
+// })
+// server.listen(Number(process.argv[2]))
